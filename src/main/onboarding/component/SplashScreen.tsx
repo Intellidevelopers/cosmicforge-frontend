ecbfdda7b894c61fddeaf2838649906aca8d7e78
alimancs <aliman2952003@gmail.com>
2025-01-25 19:38:58 +0100

    splash screen retouch

--- src/main/onboarding/component/SplashScreen.tsx
+++ src/main/onboarding/component/SplashScreen.tsx
@@ -4,8 +4,8 @@ import Loader from "../../../assets/Loader";
 
 const SplashScreen: React.FC = () => {
     return (
-        <div className="h-[100vh] w-screen flex flex-col items-center justify-end bg-white">
-            <div className="md:h-[65%] h-[55%] w-full pb-[30px] flex flex-col items-center justify-between md:justify-start gap-[70px] bg-gradient-to-b from-white to-[#272EA7]/50">
+        <div className="h-screen w-screen flex flex-col items-center justify-end bg-white">
+            <div className="md:h-[65%] fixed bottom-0 h-[55%] w-full pb-[30px] flex flex-col items-center justify-between md:justify-start gap-[70px] bg-gradient-to-b from-white to-[#272EA7]/50">
                 <div className="md:w-[400px] md:h-[100px] w-[80%] h-[100px]">
                     <img src={logo} alt="logo" width={'100%'} height={'100%'} style={{objectFit:'cover'}}/>
                 </div>
