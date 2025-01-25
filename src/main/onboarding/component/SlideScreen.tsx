f4d67afa7a020bc8785913ab9cc771651e522754
alimancs <aliman2952003@gmail.com>
2025-01-25 04:39:28 +0100

    splash and onboard slides

--- src/main/onboarding/component/SlideScreen.tsx
+++ src/main/onboarding/component/SlideScreen.tsx
@@ -27,14 +27,14 @@ const  SlideScreen: React.FC<ComponentProps> =  ( { banner } ) =>{
                 <div className='h-screen w-screen bg-black/20 fixed top-0'></div>
                 </div>
           
-            <div className='fixed flex flex-col md:gap-2 px-[10px] md:py-0 py-[10px] text-center md:justify-start justify-around items-center md:rounded-[3px] rounded-[30px] md:h-[180px] h-[300px] bottom-[5%] w-[95%] ml-[2.5%] bg-white/20 backdrop-blur-[5px]'>
+            <div className='fixed flex flex-col md:gap-2 px-[10px] md:py-0 py-[10px] text-center md:justify-start justify-around items-center md:rounded-[3px] rounded-[20px] md:h-[180px] h-[300px] bottom-[5%] w-[95%] ml-[2.5%] bg-white/20 backdrop-blur-[5px]'>
                 <div className='flex md:pl-0 h-[50px] pl-[30px] gap-1 md:mt-[20px] mt-0 flex-row w-[100%] md:justify-center justify-start items-top'>
                     <div className={`${currentSlide===0?'w-[50px] bg-[#272EA7]':'w-[25px] bg-[#272EA7]/40'} h-[3px] rounded-[20px]`}></div>
                     <div className={`${currentSlide===1?'w-[50px] bg-[#272EA7]':'w-[25px] bg-[#272EA7]/40'} h-[3px] rounded-[20px]`}></div>
                     <div className={`${currentSlide===2?'w-[50px] bg-[#272EA7]':'w-[25px] bg-[#272EA7]/40'} h-[3px] rounded-[20px]`}></div>
                     <div className={`${currentSlide===3?'w-[50px] bg-[#272EA7]':'w-[25px] bg-[#272EA7]/40'} h-[3px] rounded-[20px]`}></div>
                 </div>
-                <div className='md:text-[28px] text-[23px] font-extrabold'>
+                <div className='md:text-[28px] text-[20px] font-extrabold'>
                     <span>{banner[currentSlide].title}<span className='text-[#272EA7]'>{banner[currentSlide].highlight}</span></span>
                 </div>
                 <span className='text-white font-extrabold font-stretch-expanded md:text-[18px] text-[16px]'>{banner[currentSlide].paragraph}</span>
--- src/main/onboarding/component/SplashScreen.tsx
+++ src/main/onboarding/component/SplashScreen.tsx
@@ -4,13 +4,13 @@ import Loader from "../../../assets/Loader";
 
 const SplashScreen: React.FC = () => {
     return (
-        <div className="h-screen w-screen flex flex-col items-center justify-end bg-white">
-            <div className="md:h-[65%] h-[55%] w-full flex flex-col items-center justify-between md:justify-start gap-[70px] bg-gradient-to-b from-white to-[#272EA7]/50">
+        <div className="h-[100vh] w-screen flex flex-col items-center justify-end bg-white">
+            <div className="md:h-[65%] h-[55%] w-full pb-[30px] flex flex-col items-center justify-between md:justify-start gap-[70px] bg-gradient-to-b from-white to-[#272EA7]/50">
                 <div className="md:w-[400px] md:h-[100px] w-[80%] h-[100px]">
                     <img src={logo} alt="logo" width={'100%'} height={'100%'} style={{objectFit:'cover'}}/>
                 </div>
                 <Loader/>
-                <span className="text-[#272EA7] md:mb-0 mb-[80px] font-extrabold text-[20px] text-center ">Cosmicforge Healthnet Limited</span>
+                <span className="text-[#272EA7] md:mb-0 font-extrabold text-[18px] text-center ">Cosmicforge Healthnet Limited</span>
             </div>
         </div>
     )
