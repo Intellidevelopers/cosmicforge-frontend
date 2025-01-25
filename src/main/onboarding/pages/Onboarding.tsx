0a598c4786e135610d3f429ad40396652840a32e
alimancs <aliman2952003@gmail.com>
2025-01-25 03:52:16 +0100

    splash and onboard slides

---
+++ src/main/onboarding/pages/Onboarding.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Onboard:React.FC = () => {
+    return (
+        <div>
+            <span>Onboard page</span>
+        </div>
+    )
+}
+
+export default Onboard;
