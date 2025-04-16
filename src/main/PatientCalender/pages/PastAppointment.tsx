c0bd1e8a6c0aad626e87c85d4fcd55ca2cad834e
Devjoe <benagu477@gmail.com>
2025-04-16 14:29:00 +0100

    update on code

--- src/main/PatientCalender/pages/PastAppointment.tsx
+++ src/main/PatientCalender/pages/PastAppointment.tsx
@@ -4,7 +4,7 @@ const PastAppointment = () =>{
     return <div className="w-full p-3 ">
     <p className="font-bold ">Past Appointment</p>
     <div className="mt-4 w-full flex flex-col gap-4">
-      
+       nn  
     </div>
   </div>
 
