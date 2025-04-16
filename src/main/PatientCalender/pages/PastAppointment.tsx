97e7d9ccb6e49e4b3e8f2df814203cd0624788d9
Devjoe <benagu477@gmail.com>
2025-04-16 14:25:39 +0100

    update on code

--- src/main/PatientCalender/pages/PastAppointment.tsx
+++ src/main/PatientCalender/pages/PastAppointment.tsx
@@ -1,12 +1,10 @@
-import CustomPatientAppointmentCard from "../component/CustomPatientAppointmentCard"
+//import CustomPatientAppointmentCard from "../component/CustomPatientAppointmentCard"
 
 const PastAppointment = () =>{
     return <div className="w-full p-3 ">
     <p className="font-bold ">Past Appointment</p>
     <div className="mt-4 w-full flex flex-col gap-4">
-      <CustomPatientAppointmentCard/>
-      <CustomPatientAppointmentCard/>
-      <CustomPatientAppointmentCard/>
+      
     </div>
   </div>
 
